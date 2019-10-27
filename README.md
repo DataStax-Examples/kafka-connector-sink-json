b3d0840af177d1240664863e4d46c2a19a71df7d
csplinter <chris.splinter@datastax.com>
2019-10-27 18:39:20 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# kafka-connector-sink-json
-Example ingesting JSON records from Kafka to multiple tables in the DataStax database using the DataStax Apache Kafka Connector
+# Ingest JSON from Kafka
+This example shows how to ingest JSON records from Kafka to multiple tables in the DataStax database using the DataStax Apache Kafka Connector.
+
+## Project Layout
+- [Dockerfile-connector](Dockerfile-connector): Dockerfile to build an image of Kafka Connect with the DataStax Kafka Connector installed.
+- [Dockerfile-producer](Dockerfile-producer): Dockerfile to build an image for the producer contained in this repository.
+- [docker-compose.yml](docker-compose.yml): Uses Confluent and DataStax docker images to set up Zookeeper, Kafka Brokers, Kafka Connect, DDAC, and the producer container.
+- [connector-config.json](connector-config.json): Configuration file for the DataStax Kafka Connector to be used with the distributed Kafka Connect Worker.
+- [producer](producer/): Contains the Kafka Java Producer to write records to Kafka. Uses the StringSerializer for the Kafka record key and the JsonSerializer for the Kafka record value.
+
+## Before you begin
+#### Prerequisites
+- Docker: https://docs.docker.com/v17.09/engine/installation/
+- Docker Compose: https://docs.docker.com/compose/install/
+
+## Setup
+Clone this repository
+```
+git clone https://github.com/DataStax-Examples/kafka-connector-sink-json.git
+```
+
+Go to the directory
+```
+cd kafka-connector-sink-json
+```
+
+Build the DataStax Kafka Connector image
+```
+docker build . -t datastax-connect -f Dockerfile-connector
+```
+
+Build the JSON Java Producer image
+```
+docker build . -t kafka-producer -f Dockerfile-producer
+```
+
+Start Zookeeper, Kafka Brokers, Kafka Connect, DDAC, and the producer containers
+```
+docker-compose up -d
+```
+
+## Running
+Now that everything is up and running, it's time to set up the flow of data from Kafka to the DataStax database.
+
+Create the Kafka Topic named `json-stream` that the connector will read from.
+```
+docker exec -it kafka-broker bash
+```
+```
+kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 10 --topic json-stream --config retention.ms=-1
+```
+
+Create the DataStax tables that the connector will write to. Note that a single instance of the connector can write Kafka records to multiple tables.
+```
+docker exec -it datastax-db cqlsh
+```
+```
+create keyspace if not exists kafka_examples with replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+create table if not exists kafka_examples.stocks_table_by_symbol (symbol text, datetime timestamp, exchange text, industry text, name text, value double, PRIMARY KEY (symbol, datetime));
+create table if not exists kafka_examples.stocks_table_by_exchange (symbol text, datetime timestamp, exchange text, industry text, name text, value double, PRIMARY KEY (exchange, datetime));
+create table if not exists kafka_examples.stocks_table_by_industry (symbol text, datetime timestamp, exchange text, industry text, name text, value double, PRIMARY KEY (industry, datetime));
+```
+
+Write 1000 records ( 10 stocks, 100 records per stock ) to Kafka using the JSON Java Producer
+```
+docker exec -it kafka-producer bash
+```
+```
+mvn clean compile exec:java -Dexec.mainClass=json.JsonProducer -Dexec.args="json-stream 10 100 broker:29092"
+```
+
+Start the DataStax Kafka Connector using the Kafka Connect REST API
+```
+curl -X POST -H "Content-Type: application/json" -d @connector-config.json "http://localhost:8083/connectors"
+```
+
+Confirm that the rows were written in the DataStax database
+```
+docker exec -it datastax-db cqlsh
+```
+```
+select * from kafka_examples.stocks_table_by_symbol limit 10;
+select * from kafka_examples.stocks_table_by_exchange limit 10;
+select * from kafka_examples.stocks_table_by_industry limit 10;
+```
---
+++ producer/src/main/java/json/JsonProducer.java
@@ -0,0 +1,129 @@
+package json;
+
+import json.utils.ExchangeUtils;
+import json.ticks.TickData;
+import json.ticks.TickGenerator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+public class JsonProducer {
+
+    // Report number of records sent every this many seconds.
+    private static final long PROGRESS_REPORTING_INTERVAL = 5;
+
+    private static Logger log = LoggerFactory.getLogger("JsonProducer");
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String TOPIC = "json-stream";
+        int RECORDS_PER_STOCK = 2_000_000;
+        int NUMBER_OF_STOCKS = 500;
+        String BOOTSTRAP_SERVERS = "localhost:9092";
+
+        if (args.length == 1) {
+            TOPIC = args[0];
+        }
+        else if (args.length == 2){
+            TOPIC = args[0];
+            NUMBER_OF_STOCKS = Integer.parseInt(args[1]);
+        }
+        else if (args.length == 3){
+            TOPIC = args[0];
+            NUMBER_OF_STOCKS = Integer.parseInt(args[1]);
+            RECORDS_PER_STOCK = Integer.parseInt(args[2]);
+        }
+        else if (args.length == 4){
+            TOPIC = args[0];
+            NUMBER_OF_STOCKS = Integer.parseInt(args[1]);
+            RECORDS_PER_STOCK = Integer.parseInt(args[2]);
+            BOOTSTRAP_SERVERS = args[3];
+        }
+
+        log.info("Topic Name: {}, Number of Stocks: {}, Records per Stock: {}, Bootstrap Servers: {}",
+                TOPIC, NUMBER_OF_STOCKS, RECORDS_PER_STOCK, BOOTSTRAP_SERVERS);
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        props.put(ProducerConfig.ACKS_CONFIG, "1");
+
+        // Start a timer to measure how long this run takes overall.
+        Instant start = Instant.now();
+
+        final KafkaProducer<String, JsonNode> producer = new KafkaProducer<>(props);
+        Runtime.getRuntime().addShutdownHook(new Thread(producer::close, "Shutdown-thread"));
+
+        TickGenerator generator = new TickGenerator(ExchangeUtils.getExchangeData());
+
+        // Calculate the total number of records we expect to generate, an object
+        // to keep track of the number of errors we encounter, and a latch that
+        // will be signalled every time a "send" completes. This latch allows us
+        // to wait for all sends to complete before terminating the program.
+        int totalRecords = RECORDS_PER_STOCK * NUMBER_OF_STOCKS;
+        AtomicLong errorCount = new AtomicLong();
+        CountDownLatch requestLatch = new CountDownLatch(totalRecords);
+
+        // Create a counter to track the number of records we've successfully
+        // created so far.
+        final AtomicLong successCount = new AtomicLong();
+
+        // This callback will be invoked whenever a send completes. It reports any
+        // errors (and bumps the error-count) and signals the latch as described above.
+        Callback postSender = (recordMetadata, e) -> {
+            if (e != null) {
+                log.error("Error adding to topic", e);
+                errorCount.incrementAndGet();
+            } else {
+                successCount.incrementAndGet();
+            }
+            requestLatch.countDown();
+        };
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(
+                ()->log.info("Successfully created {} Kafka records", successCount.get()),
+                2, PROGRESS_REPORTING_INTERVAL, TimeUnit.SECONDS);
+
+        for (int j = 0; j < RECORDS_PER_STOCK; j++) {
+            for (int i = 0; i < NUMBER_OF_STOCKS; i++) {
+                TickData tickData = generator.getStockWithRandomValue(i);
+                tickData.setDatetime();
+                producer.send(new ProducerRecord<>(TOPIC, tickData.getName(), mapper.valueToTree(tickData)), postSender);
+            }
+        }
+
+        // Wait for sends to complete.
+        requestLatch.await();
+
+        // Stop the thread that periodically reports progress.
+        scheduler.shutdown();
+        producer.close();
+        long duration = Duration.between(start, Instant.now()).getSeconds();
+        log.info("Completed loading {}/{} records to Kafka in {} seconds",
+                totalRecords - errorCount.get(), totalRecords, duration);
+    }
+}
+
